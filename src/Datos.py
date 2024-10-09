9c914280f513476299be44e618da1896cfaf5d79
juanpacasm <juan.castrillonm@upb.edu.co>
2024-10-09 10:26:20 -0500

    primera funcion

--- src/Datos.py
+++ src/Datos.py
@@ -6,3 +6,14 @@ def main():
 
 if __name__ == "__main__":
     main()
+
+mensajes = ["Trabajo listo","Trabajo en proceso","Trabajo revisado","Trabajo corregido"]
+
+def elegir_mensaje():
+    print("Bienvenido al chat, estas son las opciones de mensajes: ")
+    print("")
+    print("1. Trabajo listo \n2. Trabajo en proceso \n3. Trabajo revisado \n4.Trabajo corregido ")
+    opcion = int(input("Seleccione el mensaje:  "))
+    print(mensajes[opcion-1])
+
+elegir_mensaje()
